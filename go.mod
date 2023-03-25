1243d6bc8b57920576528238b8a6948d4de47072
Itamar <itamarkn60@gmail.com>
2023-03-25 17:17:07 +0300

    feat(pkg): Making logic of CleanHTML clear all unnecessary data and tags

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/itamadev/columbus
 
 go 1.20
+
+require github.com/grokify/html-strip-tags-go v0.0.1 // indirect
--- pkg/html/html.go
+++ pkg/html/html.go
@@ -1,5 +1,21 @@
 package html
 
+import (
+	"regexp"
+	"strings"
+
+	strip "github.com/grokify/html-strip-tags-go"
+)
+
+const DOCTYPE = "<!doctype html>"
+
 func CleanHTML(html string) string {
-	return ""
+	space := regexp.MustCompile(`\s+`)
+	lower := strings.ToLower(html)
+	noDoc := strings.ReplaceAll(lower, DOCTYPE, "")
+	noTags := strip.StripTags(noDoc)
+	noSpaces := space.ReplaceAllString(noTags, " ")
+	noSpaces = strings.ReplaceAll(noSpaces, "\n\t", "")
+	cleaned := strings.TrimSpace(noSpaces)
+	return cleaned
 }
