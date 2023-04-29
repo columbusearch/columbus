a2c906c93029793ad44914a8956c8377d5645969
Itamar <itamarkn60@gmail.com>
2023-04-29 14:49:31 +0300

    chore(ui): Adding better styling to UI backgorund

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       transparent: 'transparent',
       current: 'currentColor',
       golangblue: "#74cedd",
+      white: "#ffffff",
     },
     extend: {
       backgroundImage: {
